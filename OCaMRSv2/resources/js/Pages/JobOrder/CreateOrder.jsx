022cc8e7e17e353f4dbe3854e238d82c1a8e16d6
AXDurante <125945576+AXDurante@users.noreply.github.com>
2024-08-28 22:39:53 +0800

    Updated CreateOrder front-end

--- OCaMRSv2/resources/js/Pages/JobOrder/CreateOrder.jsx
+++ OCaMRSv2/resources/js/Pages/JobOrder/CreateOrder.jsx
@@ -4,7 +4,7 @@ function CreateOrder({ jobOrder }) {
     console.log(jobOrder)
     return (
         <>
-            <div className="d-flex"> 
+            <div className="d-flex">
                 <div id="content" className="main-content flex-fill p-3">
                     <div>
                         <div>
@@ -12,84 +12,136 @@ function CreateOrder({ jobOrder }) {
                             <h1 class="d-inline fw-light">Open Request</h1>
                             <hr />
                         </div>
-                        <div>
+                        <div className="mt3">
                             <h4>Information</h4>
                             <div className="row forms-bg p-3">
-
                                 <div className="col d-flex flex-column align-items-center p-3">
-                                    
-                                    <h6 className="d-flex flex-column align-items-start w-100">
+                                    <h6 className="d-flex flex-column align-items-start fw-bold mt-2 w-100">
                                         Service Requested
                                     </h6>
-                                    <select
-                                        class="form-select"
+                                    <input
                                         type="text"
-                                        className="d-flex flex-column align-items-center w-100 mb-2">
-                                            <option value="" disabled selected> Please select type of service </option>
-                                            <option> Option 1 </option>
-                                            <option> Option 2 </option>
-                                            <option> Option 3 </option>
-                                    </select>
-
-                                    <h6 className="d-flex flex-column align-items-start w-100">
+                                        className="d-flex flex-column align-items-center w-100 rounded"
+                                    />
+                                    <h6 className="d-flex flex-column align-items-start fw-bold mt-2 w-100">
                                         Laboratory
                                     </h6>
                                     <input
                                         type="text"
-                                        className="d-flex flex-column align-items-center w-100 mb-2"
+                                        className="d-flex flex-column align-items-center w-100 rounded"
                                     />
-
-                                    <h6 className="d-flex flex-column align-items-start w-100">
-                                        College/Faculty/Office
+                                    <h6 className="d-flex flex-column align-items-start fw-bold mt-2 w-100">
+                                        College/ Faculty / Office
                                     </h6>
-                                    <select
-                                        class="form-select"
+                                    <input
                                         type="text"
-                                        className="d-flex flex-column align-items-center w-100 mb-2">
-                                            <option value="" disabled selected> Select your College/Faculty/Office </option>
-                                            <option> Option 1 </option>
-                                            <option> Option 2 </option>
-                                            <option> Option 3 </option>
-                                    </select>
+                                        className="d-flex flex-column align-items-center w-100 rounded"
+                                    />
                                 </div>
-
-                                <div className="col d-flex flex-column align-items-center p-3">
-                                    <h6 className="d-flex flex-column align-items-start w-100">
-                                        Instrument Transportation
+                                <div className="col d-flex flex-column align-items-center  p-3">
+                                    <h6 className="d-flex flex-column align-items-start fw-bold mt-2 w-100">
+                                        Instrumentation Transportation
                                     </h6>
-                                    <select
-                                        class="form-select"
+                                    <input
                                         type="text"
-                                        className="d-flex flex-column align-items-center w-100 mb-2">
-                                            <option value="" disabled selected> Please select type of service </option>
-                                            <option> Option 1 </option>
-                                            <option> Option 2 </option>
-                                            <option> Option 3 </option>
-                                    </select>
-
-                                    <h6 className="d-flex flex-column align-items-start w-100">
+                                        className="d-flex flex-column align-items-center w-100 rounded"
+                                    />
+                                    <h6 className="d-flex flex-column align-items-start fw-bold mt-2 w-100">
                                         Laboratory Location
                                     </h6>
                                     <input
                                         type="text"
-                                        className="d-flex flex-column align-items-center w-100 mb-2"
+                                        className="d-flex flex-column align-items-center w-100 rounded"
                                     />
-
-                                    <h6 className="d-flex flex-column align-items-start w-100">
+                                    <h6 className="d-flex flex-column align-items-start  fw-bold mt-2 w-100">
                                         Position
                                     </h6>
                                     <input
                                         type="text"
-                                        className="d-flex flex-column align-items-center w-100 mb-2"
+                                        className="d-flex flex-column align-items-center w-100 rounded"
                                     />
-                                </div>
-
-                                <div className="row">
-                                    <h6 className="ms-1"> Remarks </h6>
-                                    <textarea className="mx-3" placeholder="Enter remarks"/>
                                 </div>
                             </div>
                         </div>
+                    </div>
+
+                    <div>
+                        <div className="">
+                            <h4 className="mt-4">Item No.1</h4>
+                            <div className="row forms-bg p-3">
+                                <div className="row forms-bg p-3">
+                                    <div className="col-12 col-md-5 d-flex flex-column p-3">
+                                        <h6 className="w-100 fw-bold text-start">
+                                            Instrument
+                                        </h6>
+                                        <input
+                                            type="text"
+                                            className="w-100 mb-2 rounded"
+                                        />
+                                        <h6 className="w-100 fw-bold text-start">
+                                            Model
+                                        </h6>
+                                        <input
+                                            type="text"
+                                            className="w-100 mb-2 rounded"
+                                        />
+                                        <h6 className="w-100 fw-bold text-start">
+                                            Image Attachment
+                                        </h6>
+                                        <button className="btn btn-secondary w-50">
+                                            + Insert Image
+                                        </button>
+                                    </div>
+
+                                    <div className="col-12 col-md-3 d-flex flex-column p-3">
+                                        <h6 className="w-100 fw-bold text-start">
+                                            Quantity
+                                        </h6>
+                                        <input
+                                            type="text"
+                                            className="w-50 mb-2 justify-content-start rounded"
+                                        />
+                                        <h6 className="w-100 fw-bold text-start">
+                                            Manufacturer
+                                        </h6>
+                                        <input
+                                            type="text"
+                                            className="w-100 mb-2 rounded"
+                                        />
+                                    </div>
+
+                                    <div className="col-12 col-md-4 d-flex flex-column p-3">
+                                        <h6 className="w-100 fw-bold text-start ">
+                                            Instrument Serial No
+                                        </h6>
+                                        <input
+                                            type="text"
+                                            className="w-100 fw-bold mb-2 rounded"
+                                        />
+                                        <h6 className="w-100 fw-bold text-start">
+                                            Property
+                                        </h6>
+                                        <input
+                                            type="text"
+                                            className="w-100 mb-2 rounded"
+                                        />
+                                    </div>
+
+                                    <div className="col-12 d-flex flex-row-reverse">
+                                        <button className="btn btn-danger">
+                                            delete
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                            <button className="jb-btn-add mt-3 mb-2">
+                                Add More Instrument
+                            </button>
+                        </div>
+                        <hr />
+                        <button className="jb-btn-submit w-100 mt-3">
+                            Submit Job Order
+                        </button>
                     </div>
                 </div>
             </div>
