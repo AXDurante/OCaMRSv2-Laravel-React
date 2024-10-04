a9d5377fa3eb5b41fd27e87444044e1594d1b493
Simowwn <125945116+Simowwn@users.noreply.github.com>
2024-10-04 19:10:58 +0800

    Fix pdf TSR

--- OCaMRSv2/resources/js/Pages/Tech/TSRpdf.jsx
+++ OCaMRSv2/resources/js/Pages/Tech/TSRpdf.jsx
@@ -6,6 +6,7 @@ import {
     StyleSheet,
     View,
     Font,
+    Image,
 } from "@react-pdf/renderer";
 
 // Register fonts
@@ -23,12 +24,39 @@ Font.register({
 });
 Font.register({
     family: "Arial",
-    src: "/fonts/ARIALBD 1.ttf", // Register the bold font
+    src: "/fonts/ARIALBD 1.ttf",
     fontWeight: "bold",
+});
+Font.register({
+    family: "Arial",
+    src: "/fonts/ARIALI.ttf",
+    fontStyle: "italic",
 });
 
 // Create styles for common elements
 const styles = StyleSheet.create({
+    headerContainer: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+        marginTop: 10,
+    },
+    headerText: {
+        textAlign: "center",
+        fontSize: 15,
+        fontFamily: "Georgia",
+        marginTop: 10,
+    },
+    subHeaderText: {
+        textAlign: "center",
+        fontSize: 12,
+        fontFamily: "Cambria Math",
+        marginTop: 0, // Set marginTop to 0
+    },
+    logo: {
+        width: 70,
+        height: 70,
+    },
     textStyle: {
         fontSize: 9,
     },
@@ -89,35 +117,46 @@ const TSRpdf = () => {
     return (
         <Document>
             <Page size={[8.5 * 72, 13 * 72]}>
-                {" "}
-                {/* 1 inch = 72 points */}
-                {/* Header Texts */}
+                <View style={styles.headerContainer}>
+                    <Image
+                        src="/images/UstLogo.png"
+                        style={[styles.logo, { marginLeft: 50 }]}
+                    />
+                    <View
+                        style={{
+                            flexDirection: "column",
+                            alignItems: "center",
+                        }}
+                    >
+                        <Text style={styles.headerText}>
+                            U&nbsp;N&nbsp;I&nbsp;V&nbsp;E&nbsp;R&nbsp;S&nbsp;I&nbsp;T&nbsp;Y&nbsp;
+                            &nbsp;O&nbsp;F&nbsp;
+                            &nbsp;S&nbsp;A&nbsp;N&nbsp;T&nbsp;O&nbsp;
+                            &nbsp;T&nbsp;O&nbsp;M&nbsp;A&nbsp;S
+                        </Text>
+                        <Text style={styles.subHeaderText}>
+                            LABORATORY EQUIPMENT AND SUPPLIES OFFICE
+                        </Text>
+                    </View>
+                    <Image
+                        src="/images/LesoLogo.jpg"
+                        style={[styles.logo, { marginRight: 50 }]}
+                    />
+                </View>
+
                 <Text
-                    style={{
-                        textAlign: "center",
-                        fontSize: 15,
-                        fontFamily: "Georgia",
-                        marginTop: 30,
-                    }}
+                    style={{ textAlign: "center", fontSize: 12, marginTop: 10 }}
                 >
-                    U&nbsp;N&nbsp;I&nbsp;V&nbsp;E&nbsp;R&nbsp;S&nbsp;I&nbsp;T&nbsp;Y&nbsp;&nbsp;O&nbsp;F&nbsp;&nbsp;S&nbsp;A&nbsp;N&nbsp;T&nbsp;O&nbsp;&nbsp;T&nbsp;O&nbsp;M&nbsp;A&nbsp;S
+                    INSTRUMENTATION SERVICE CENTER
                 </Text>
                 <Text
                     style={{
                         textAlign: "center",
                         fontSize: 12,
-                        fontFamily: "Cambria Math",
+                        marginTop: 15,
+                        fontFamily: "Arial",
+                        fontWeight: "bold",
                     }}
-                >
-                    LABORATORY EQUIPMENT AND SUPPLIES OFFICE
-                </Text>
-                <Text
-                    style={{ textAlign: "center", fontSize: 12, marginTop: 30 }}
-                >
-                    INSTRUMENTATION SERVICE CENTER
-                </Text>
-                <Text
-                    style={{ textAlign: "center", fontSize: 12, marginTop: 10 }}
                 >
                     TECHNICAL SERVICE REPORT
                 </Text>
@@ -128,6 +167,8 @@ const TSRpdf = () => {
                         fontSize: 12,
                         marginTop: 10,
                         marginRight: 150,
+                        fontFamily: "Arial",
+                        fontWeight: "bold",
                     }}
                 >
                     TSR No.
@@ -135,8 +176,10 @@ const TSRpdf = () => {
                 <Text
                     style={{
                         textAlign: "right",
-                        fontSize: 12,
-                        marginRight: 110,
+                        fontSize: 9,
+                        marginRight: 130,
+                        fontFamily: "Arial",
+                        fontStyle: "italic",
                     }}
                 >
                     (To be filled by LESO)
@@ -244,7 +287,13 @@ const TSRpdf = () => {
                         style={[
                             styles.alignLeft,
                             styles.textStyle,
-                            { fontFamily: "Arial", fontWeight: "bold" },
+                            {
+                                fontFamily: "Arial",
+                                fontWeight: "bold",
+                                marginTop: 10,
+                                marginBottom: 3,
+                                marginLeft: 3,
+                            },
                         ]}
                     >
                         RECOMMENDATION
@@ -254,7 +303,6 @@ const TSRpdf = () => {
                             flexDirection: "row",
                             alignItems: "center",
                             marginTop: 5,
-                            marginLeft: 10,
                         }}
                     >
                         <View
@@ -265,7 +313,8 @@ const TSRpdf = () => {
                                 borderColor: "#000",
                                 alignItems: "center",
                                 justifyContent: "center",
-                                marginRight: 5,
+                                marginRight: 10,
+                                marginLeft: 5,
                             }}
                         >
                             {/* Checkbox for "For Pull-out" */}
@@ -331,6 +380,7 @@ const TSRpdf = () => {
                                 fontFamily: "Arial",
                                 fontWeight: "bold",
                                 marginTop: 15,
+                                marginLeft: 9,
                             },
                         ]}
                     >
@@ -341,6 +391,7 @@ const TSRpdf = () => {
                             borderBottomWidth: 2,
                             borderBottomColor: "black",
                             marginTop: 15,
+                            marginLeft: 9,
                         }}
                     />
                     <View
@@ -348,27 +399,70 @@ const TSRpdf = () => {
                             borderBottomWidth: 2,
                             borderBottomColor: "black",
                             marginTop: 15,
+                            marginLeft: 9,
                         }}
                     />
-                    <Text
-                        style={[
-                            styles.alignLeft,
-                            styles.textStyle,
-                            {
-                                fontFamily: "Arial",
-                                fontWeight: "bold",
-                                marginTop: 15,
-                            },
-                        ]}
+                    <View
+                        style={{
+                            flexDirection: "row",
+                            marginTop: 15,
+                            marginLeft: 9,
+                        }}
                     >
-                        SERVICE PERFORMED BY: ___________________________ Noted
-                        by: ___________________________
-                    </Text>
-                    <Text style={[styles.alignLeft, styles.textStyle]}>
-                        Instrumentation Technician / Date LESO Administrator /
-                        Date
-                    </Text>
+                        <Text
+                            style={[
+                                styles.alignLeft,
+                                styles.textStyle,
+                                {
+                                    fontFamily: "Arial",
+                                    fontWeight: "bold",
+                                },
+                            ]}
+                        >
+                            SERVICE PERFORMED BY:
+                            ___________________________________
+                        </Text>
+                        <Text
+                            style={[
+                                styles.alignLeft,
+                                styles.textStyle,
+                                {
+                                    fontFamily: "Arial",
+                                    fontWeight: "bold",
+                                    marginLeft: 20, // Adjust the marginLeft to provide spacing between the two elements
+                                },
+                            ]}
+                        >
+                            Noted by: ________________________
+                        </Text>
+                    </View>
 
+                    <View
+                        style={{
+                            flexDirection: "row",
+                            marginTop: 2,
+                            marginLeft: 9,
+                        }}
+                    >
+                        <Text
+                            style={[
+                                styles.alignLeft,
+                                styles.textStyle,
+                                { marginLeft: 138 },
+                            ]}
+                        >
+                            Instrumentation Technician / Date
+                        </Text>
+                        <Text
+                            style={[
+                                styles.alignLeft,
+                                styles.textStyle,
+                                { marginLeft: 90 },
+                            ]}
+                        >
+                            LESO Administrator / Date
+                        </Text>
+                    </View>
                     <Text
                         style={[
                             styles.alignLeft,
@@ -377,70 +471,99 @@ const TSRpdf = () => {
                                 fontFamily: "Arial",
                                 fontWeight: "bold",
                                 marginTop: 15,
+                                marginLeft: 9,
                             },
                         ]}
                     >
                         SERVICE ACKNOWLEDGEMENT:
                     </Text>
-                    <Text style={[styles.alignLeft, styles.textStyle]}>
+                    <Text
+                        style={[
+                            styles.alignLeft,
+                            styles.textStyle,
+                            {
+                                marginLeft: 9,
+                                marginBottom: 9,
+                                marginTop: 2,
+                            },
+                        ]}
+                    >
                         This is to acknowledge that the above service has been
                         performed and completed in our laboratory/office.
                     </Text>
+
+                    <View
+                        style={{
+                            flexDirection: "row",
+                            marginTop: 2,
+                        }}
+                    >
+                        <Text
+                            style={[
+                                styles.textStyle,
+                                { fontFamily: "Arial", fontWeight: "bold" },
+                            ]}
+                        >
+                            REQUESTED BY:
+                            ___________________________________________
+                        </Text>
+                        <Text
+                            style={[
+                                styles.alignLeft,
+                                styles.textStyle,
+                                { marginLeft: 70 },
+                            ]}
+                        >
+                            E-mail: __________________
+                        </Text>
+                    </View>
 
                     <Text
                         style={[
                             styles.alignLeft,
                             styles.textStyle,
-                            { fontFamily: "Arial", fontWeight: "bold" },
+                            { marginLeft: 90, marginTop: 2 },
                         ]}
                     >
-                        REQUESTED BY:
-                    </Text>
-                    <Text style={[styles.alignLeft, styles.textStyle]}>
-                        REQUESTED BY:
-                        ___________________________________________ E-mail:
-                        ___________________________
-                    </Text>
-                    <Text style={[styles.alignLeft, styles.textStyle]}>
                         Signature over printed name / Date
                     </Text>
 
                     <Text
                         style={[
-                            styles.alignLeft,
                             styles.textStyle,
-                            { fontFamily: "Arial", fontWeight: "bold" },
+                            {
+                                fontFamily: "Arial",
+                                fontWeight: "bold",
+                                marginTop: 4,
+                            },
                         ]}
                     >
-                        POSITION:
-                    </Text>
-                    <Text style={[styles.alignLeft, styles.textStyle]}>
                         POSITION:
                         _______________________________________________
                     </Text>
 
-                    <Text style={[styles.alignRight, styles.textStyle]}>
+                    <Text
+                        style={[
+                            styles.alignRight,
+                            styles.textStyle,
+                            {
+                                marginbt: 30,
+                                marginTop: 30,
+                                fontFamily: "Arial",
+                                fontStyle: "italic",
+                            },
+                        ]}
+                    >
                         UST:S022-00-FO34 rev01 05/02/23
                     </Text>
                 </View>
-                <View style={[styles.mL50, styles.alignLeft]}>
-                    <Text style={{ fontSize: 8 }}>
-                        {" "}
-                        {/* Set font size to 8 */}
-                        8/F UST Central Laboratory Building, España Boulevard,
-                        Sampaloc, Manila, 1015 Philippines
-                    </Text>
-                    <Text style={{ fontSize: 8 }}>
-                        {" "}
-                        {/* Set font size to 8 */}
-                        Tel. No: +632-8712-6349;+632-3406-1611 local 8266
-                    </Text>
-                    <Text style={{ fontSize: 8 }}>
-                        {" "}
-                        {/* Set font size to 8 */}
-                        Email address: leso@ust.edu.ph
-                    </Text>
-                </View>
+                <Image
+                    src="/images/TSRFooter.png"
+                    style={[
+                        styles.logo,
+                        { width: "100%", height: "auto", marginTop: 30 },
+                    ]} // Updated to full width
+                />
             </Page>
         </Document>
     );
